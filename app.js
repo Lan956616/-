8b36bc22a604062b438b1e6cfa221417b235e12d
Lan956616 <cd25924218@gmail.com>
2024-03-26 18:05:03 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req,res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req,res) => {
   res.send('listing restaurant')
+})
+
+app.get('/restaurant/:id', (req,res) => {
+  const id = req.params.id
+  res.send(`get restaurant ${id}`)
 })
 
 app.listen(port, () => {
